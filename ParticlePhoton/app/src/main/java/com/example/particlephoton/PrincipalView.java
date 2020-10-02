5dbb2faf28156c91e2b6a0e46136d741b9837b51
Arjun Verma <averma332@gatech.edu>
2020-10-02 17:04:24 -0400

    More Backend

---
+++ ParticlePhoton/app/src/main/java/com/example/particlephoton/PrincipalView.java
@@ -0,0 +1,9 @@
+package com.example.particlephoton;
+
+import android.content.Context;
+
+public class PrincipalView extends BaseView {
+    public PrincipalView(Context context) {
+        super(context);
+    }
+}
